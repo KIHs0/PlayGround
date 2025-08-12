b0972d084c698842c1ef56abfe81f9b3d6d90eab
oshik <kihsogaming@gmail.com>
2025-08-12 13:07:58 +0545

    see

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-
 import express from "express";
 import cors from "cors";
 import router from "./routes/router.dallE.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 app.use("/", router);
 
 app.listen(PORT, () => {
