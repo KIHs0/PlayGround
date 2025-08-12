96055328afa44d56c1350694ac225cd415a2dc96
oshik <kihsogaming@gmail.com>
2025-08-12 15:07:48 +0545

    redo configjs

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,8 +2,5 @@ const config = {
   development: {
     backendUrl: "/generate",
   },
-  production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
-  },
 };
 export default config;
