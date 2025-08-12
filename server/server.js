62ba7d65b74d5158e3c574240fa7bbb0de00c6a7
oshik <kihsogaming@gmail.com>
2025-08-12 15:06:55 +0545

    regexpath err

--- server/server.js
+++ server/server.js
@@ -3,19 +3,18 @@ import cors from "cors";
 // import router from "./routes/router.dallE.js";
 import path from "path";
 import { fileURLToPath } from "url";
-
-const folder = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.use(cors());
 app.use(express.json());
-// app.use("/", router);
-
+let folder = path.join(path.dirname(fileURLToPath(import.meta.url)));
 app.use(express.static(path.join(folder, "client", "dist")));
-app.get("*", (req, res) => {
+// app.get("*", (req, res) => {
+//   // res.sendFile(path.join(folder, "client", "dist", "index.html"));
+// });
+app.all("/{*any}", (req, res, next) => {
   res.sendFile(path.join(folder, "client", "dist", "index.html"));
 });
-
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
