58d8a82d9d31ea5c00a78956de15069a5f58ebfc
oshik <kihsogaming@gmail.com>
2025-08-12 13:28:52 +0545

    fixed dirname

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/router.dallE.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
+const folder = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.use(express.static(path.join(__dirname, "client", "dist")));
+app.use(express.static(path.join(folder, "client", "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(folder, "client", "dist", "index.html"));
 });
 app.use("/", router);
 
