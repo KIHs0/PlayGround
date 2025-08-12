48540cbf1e913f7d2927021d919711b0595c26d2
oshik <kihsogaming@gmail.com>
2025-08-12 13:22:04 +0545

    see3

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(express.static(path.join(__dirname, "dist")));
+
+app.use(express.static(path.join(__dirname, "client", "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 app.use("/", router);
 
