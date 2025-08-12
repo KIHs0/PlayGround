2bbb837b210846bd23ab0a637a17932da4cc4ce5
oshik <kihsogaming@gmail.com>
2025-08-12 13:39:48 +0545

    see4

--- server/routes/router.dallE.js
+++ server/routes/router.dallE.js
@@ -14,20 +14,19 @@ router.get("/", (req, res) => {
 });
 
 // POST endpoint to generate image
-router.post("/generate", async (req, res) => {
-  console.log(req.body);
-  const { prompt } = req.body;
-  try {
-    const response = await openai.images.generate({
-      prompt,
-      n: 1,
-      size: "512x512",
-      response_format: "b64_json",
-    });
-    res.json({ image: response.data[0].b64_json });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
+// router.post("/generate", async (req, res) => {
+//   console.log(req.body);
+//   const { prompt } = req.body;
+//   try {
+//     const response = await openai.images.generate({
+//       prompt,
+//       n: 1,
+//       size: "512x512",
+//       response_format: "b64_json",
+//     });
+//     res.json({ image: response.data[0].b64_json });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 export default router;
