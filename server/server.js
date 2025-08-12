8290b49ec56250a17c18a29331ad5cd7f6a292ee
oshik <kihsogaming@gmail.com>
2025-08-12 13:34:52 +0545

    err

--- server/server.js
+++ server/server.js
@@ -9,13 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use("/", router);
 
 app.use(express.static(path.join(folder, "client", "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(folder, "client", "dist", "index.html"));
 });
-app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
